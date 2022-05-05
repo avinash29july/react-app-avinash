22ed3e6c229737f37f91472a3915b3c4c78d80bf
AVINASH <avinash29july@gmail.com>
2022-05-05 12:39:41 +0530

    code added

---
+++ react-app-Avinash/src/App.js
@@ -0,0 +1,12 @@
+import AddComponent from './add/AddComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AddComponent />
+    </div>
+  );
+}
+
+export default App;
